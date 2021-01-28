c158337f49ea0f1fa28531154e8a20a970430bdc
Max Forssén <max.forssen@mentimeter.com>
2021-01-28 10:28:19 +0100

    local code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
 import { Products } from 'components/Products'
 import { Cart } from 'components/Cart'
 
+import { products } from 'reducers/products'
+import { cart } from 'reducers/cart'
+
+const reducer = combineReducers({
+  cart: cart.reducer,
+  products: products.reducer
+})
+
+const store = configureStore({ reducer })
+
 export const App = () => (
-  <>
+  <Provider store={store}>
     <Cart />
     <Products />
-  </>
+  </Provider>
 )
